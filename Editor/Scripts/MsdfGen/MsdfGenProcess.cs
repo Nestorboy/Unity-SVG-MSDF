a765df64b20ff53ae3bd64ac43d4cc2e0f2efa3a
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2026-08-01 01:04:56 +0200

    Add a msdfgen process wrapper

---
+++ Editor/Scripts/MsdfGen/MsdfGenProcess.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Nessie.MSDF
+{
+    public class MsdfGenProcess
+    {
+        private readonly Process _msdfGenProcess = new();
+
+        public MsdfGenProcess()
+        {
+            StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            StartInfo.CreateNoWindow = true;
+            StartInfo.UseShellExecute = false;
+            StartInfo.FileName = GetAbsoluteMsdfGenPath();
+
+            _msdfGenProcess.EnableRaisingEvents = true;
+        }
+
+        private ProcessStartInfo StartInfo => _msdfGenProcess.StartInfo;
+
+        public MsdfGenArguments Arguments { get; } = new();
+
+        private static string GetLocalMsdfGenPath() => Path.Combine("Packages", "sh.nessie.svg-msdf", "Editor", "bin", "msdfgen.exe");
+
+        private static string GetAbsoluteMsdfGenPath() => Path.GetFullPath(GetLocalMsdfGenPath());
+
+        public void StartAndWaitForExit()
+        {
+            StartInfo.Arguments = Arguments.ToCommandString();
+
+            _msdfGenProcess.Start();
+            _msdfGenProcess.WaitForExit();
+        }
+    }
+}
