10d5fe5b5f66d099c6c9b878beecf9d85842f7b8
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2026-08-01 01:05:35 +0200

    Add msdfgen svg conversion functions

---
+++ Editor/Scripts/MsdfUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace Nessie.MSDF
+{
+    public static class MsdfUtils
+    {
+        [PublicAPI]
+        public static Texture2D SvgToSdf(string svgLocalPath, int width, int height) => ConvertSvg(svgLocalPath, width, height, GeneratorMode.SDF);
+
+        [PublicAPI]
+        public static Texture2D SvgToMsdf(string svgLocalPath, int width, int height) => ConvertSvg(svgLocalPath, width, height, GeneratorMode.MSDF);
+
+        [PublicAPI]
+        public static Texture2D ConvertSvg(string svgLocalPath, int width, int height, GeneratorMode mode)
+        {
+            var tempDirectory = GetTemporaryDirectory();
+            EnsurePathExists(tempDirectory);
+
+            var svgAbsolutePath = Path.GetFullPath(svgLocalPath);
+            var tempPngPath = GetTemporaryTexturePath();
+
+            RunConversionProcess(svgAbsolutePath, tempPngPath, width, height, mode);
+
+            var sdfTexture = new Texture2D(width, height, GetFormat(mode), TextureCreationFlags.None);
+            sdfTexture.LoadImage(File.ReadAllBytes(tempPngPath), false);
+
+            Directory.Delete(tempDirectory, true);
+
+            return sdfTexture;
+        }
+
+        private static void RunConversionProcess(string svgAbsolutePath, string pngAbsolutePath, int width, int height, GeneratorMode mode)
+        {
+            var msdfProcess = new MsdfGenProcess();
+            var args = msdfProcess.Arguments;
+            args.InputPath = svgAbsolutePath;
+            args.OutputPath = pngAbsolutePath;
+            args.Mode = mode;
+            args.Width = width;
+            args.Height = height;
+
+            msdfProcess.StartAndWaitForExit();
+        }
+
+        private static string GetTemporaryDirectory() => Path.Combine(Path.GetTempPath(), "NessieSvgMsdf");
+
+        private static string GetTemporaryTexturePath() => Path.Combine(GetTemporaryDirectory(), "result.png");
+
+        private static GraphicsFormat GetFormat(GeneratorMode mode)
+        {
+            return mode switch
+            {
+                GeneratorMode.SDF => GraphicsFormat.R8_UNorm,
+                GeneratorMode.MSDF or GeneratorMode.PSDF or GeneratorMode.MTSDF => GraphicsFormat.R8G8B8A8_UNorm,
+                _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null),
+            };
+        }
+
+        private static void EnsurePathExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
