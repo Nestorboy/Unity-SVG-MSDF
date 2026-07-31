3a7860f92584fd5eda8a6237052db2b7c5b2d28a
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2026-08-01 00:21:22 +0200

    Add a msdfgen argument wrapper class

---
+++ Editor/Scripts/MsdfGen/GeneratorMode.cs
@@ -0,0 +1,12 @@
+﻿namespace Nessie.MSDF
+{
+    public enum GeneratorMode
+    {
+        // ReSharper disable InconsistentNaming
+        SDF,
+        PSDF,
+        MSDF,
+        MTSDF,
+        // ReSharper restore InconsistentNaming
+    }
+}
---
+++ Editor/Scripts/MsdfGen/MsdfGenArguments.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Nessie.MSDF
+{
+    /// <summary>
+    /// Wrapper for msdfgen CLI arguments.
+    /// </summary>
+    public class MsdfGenArguments
+    {
+        /// <summary>
+        /// Absolute input of the input SVG.
+        /// </summary>
+        public string InputPath;
+
+        /// <summary>
+        /// Absolute path of the output PNG.
+        /// </summary>
+        public string OutputPath;
+
+        public GeneratorMode Mode = GeneratorMode.MSDF;
+        public int Width = 512;
+        public int Height = 512;
+        public float Range = 4;
+
+        /// <summary>
+        /// Combines arguments into formatted command.
+        /// </summary>
+        /// <returns>Formatted CLI command.</returns>
+        public string ToCommandString()
+        {
+            StringBuilder args = new();
+
+            string genMode = Mode.ToString().ToLowerInvariant();
+            args.Append($"\"{genMode}\" ");
+            args.Append($"-svg \"{InputPath}\" ");
+            args.Append($"-o \"{OutputPath}\" ");
+            args.Append($"-dimensions {Width} {Height} ");
+            args.Append($"-range {Range} ");
+            args.Append("-format png ");
+            args.Append("-autoframe ");
+
+            return args.ToString();
+        }
+    }
+}
